3ab3ed27aed61e255e000677673ff30664217eae
gregorycooper <gregcoop242@gmail.com>
2026-01-18 22:10:38 -0500

    Insert Sort with user Input

--- insertSort.py
+++ insertSort.py
@@ -1,10 +1,10 @@
+#Insert Sort in Monotonically Decreasing Order with input array
 def insertion_sort_decreasing(arr):
 
 
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
@@ -15,7 +15,12 @@ def insertion_sort_decreasing(arr):
 
 
 if __name__ == "__main__":
-    array = [74, 32, 10000, 19, 1, 3]
+    try:
+        array = [int(x) for x in input("Enter whole numbers seperated by commas: ").split(",")]
+    except ValueError:
+        print("Invalid input. Please enter whole numbers only.")
+        exit(1)
+    #array = [74, 32, 10000, 19, 1, 3]
     print("Original array:", array)
 
     sorted_array = insertion_sort_decreasing(array)
